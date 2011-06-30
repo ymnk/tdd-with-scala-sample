786b4a8b8b6a64d88c0fac1bba948c35b58a0526
Atsuhiko Yamanaka <atsuhiko.yamanaka@gmail.com>
2011-06-30 23:16:20 +0900

    using eclipsify.

--- project/build/MyProject.scala
+++ project/build/MyProject.scala
@@ -1,6 +1,7 @@
 import sbt._
+import de.element34.sbteclipsify._
 
-class MyProject(info: ProjectInfo) extends DefaultProject(info) {
+class MyProject(info: ProjectInfo) extends DefaultProject(info) with Eclipsify {
   val scalatest = "org.scalatest" %% "scalatest" % "1.6.1"
   val junit4 = "junit" % "junit" % "4.8"
   val junitInterface = "com.novocode" % "junit-interface" % "0.7"
